9dbce7232819c720837080d04b907188dc475956
Beardography <ryan.l.huber@gmail.com>
2023-01-17 20:38:01 -0600

    updated random colors

--- index.js
+++ index.js
@@ -1,25 +1,13 @@
-let red = Math.ceil(Math.random() * 255);
-let green = Math.ceil(Math.random() * 255);
-let blue = Math.ceil(Math.random() * 255);
-
 for (let i = 0; i < 5; i++) {
-    const div = document.createElement("div");
-    const color = `rgb(${red}, ${green}, ${blue})`;
-    div.className = "square";
+    const red = Math.ceil(Math.random() * 255);
+    const blue = Math.ceil(Math.random() * 255);
+    const green = Math.ceil(Math.random() * 255);
 
-    console.log(color);
+    color = `rgb(${red}, ${green}, ${blue})`;
+
+    const div = document.createElement('div');
+    div.className = 'square';
+
     div.style.backgroundColor = color;
-    document.body.append(div);
+    document.body.appendChild(div);
 }
-
-// for (let j = 0; j < 5; j++) {
-//     div.style.backgroundColor = color;
-// }
-
-function Palette(name) {
-    this.name = name;
-}
-
-const P1 = new Palette("Cool colors");
-
-console.log(P1.name);
