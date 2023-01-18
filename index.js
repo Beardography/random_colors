b4d37e9eddae59998ac2284e101729c3b347c400
Beardography <ryan.l.huber@gmail.com>
2023-01-17 20:49:38 -0600

    made color values to a string

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 for (let i = 0; i < 5; i++) {
-    const red = Math.ceil(Math.random() * 255);
-    const blue = Math.ceil(Math.random() * 255);
-    const green = Math.ceil(Math.random() * 255);
+    const red = Math.ceil(Math.random() * 255).toString();
+    const blue = Math.ceil(Math.random() * 255).toString();
+    const green = Math.ceil(Math.random() * 255).toString();
 
     color = `rgb(${red}, ${green}, ${blue})`;
 
